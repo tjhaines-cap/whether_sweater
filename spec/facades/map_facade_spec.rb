37756f72c89f7745aca3b4116b9d7d6eb323009a
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-06 17:23:01 -0600

    Feat: create map facade and return latitude and longitude given a city

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe MapFacade do
 
   it 'returns the latitude and longitude when given a city', :vcr do
     lat_lng = MapFacade.get_latitude_longitude("Manitou Springs, CO")
-
-    expect(lat_lng[:latitude])to eq(38.857568)
-    expect(lat_lng[:longitude]).to have_key(-104.912311)
+    
+    expect(lat_lng[:latitude]).to eq(38.857568)
+    expect(lat_lng[:longitude]).to eq(-104.912311)
   end
 end
