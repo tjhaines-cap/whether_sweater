509d4d8fc94546adc72872535777daf1e62b3ea2
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-07 08:52:17 -0600

    Feat: render json with weather infomormation for forecast request

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,9 +1,9 @@
-class API::V1::ForecastsController < ApplicationController
+class Api::V1::ForecastsController < ApplicationController
 
   def index
     lat_lng = MapFacade.get_latitude_longitude(params[:location])
     weather = WeatherFacade.get_weather(lat_lng[:latitude], lat_lng[:longitude])
-    # render json
+    render json: {data: weather}
   end
 
 end
