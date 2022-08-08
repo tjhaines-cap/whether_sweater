ff8593273c7161e0dd5c82e5d5f5f5fedaead520
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-08 13:00:21 -0600

    Test: post users request

---
+++ spec/requests/users/post_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Posts" do
+
+  describe "POST /users" do
+    it 'returns data hash with user email, api_key, and id', :vcr do
+      post '/api/v1/users', params: {
+                              email: "whatever@example.com",
+                              password: "password",
+                              password_confirmation: "password"
+    }.to_json
+binding.pry
+      user_data = JSON.parse(response.body, symbolize_names: true)
+      user = user_data[:data]
+      expect(user[:type]).to eq("users")
+      expect(user[:id]).to be_a(Integer)
+      expect(user[:attributes][:email]).to be_a(String)
+      expect(user[:attributes][:api_key]).to be_a(String)
+    end
+  end
+end
