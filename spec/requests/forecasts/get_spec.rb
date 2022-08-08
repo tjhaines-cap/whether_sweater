aca47668c9150fe8c0803f596110415bd3d6158b
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-08 13:14:39 -0600

    Test: update test for correct response with arrays in daily and hourly weather

--- spec/requests/forecasts/get_spec.rb
+++ spec/requests/forecasts/get_spec.rb
@@ -4,20 +4,23 @@ RSpec.describe "Gets" do
 
   describe "GET /forecasts" do
     it "returns forecast informaiton", :vcr do
-      get "/api/v1/forecasts?location=denver,co"
+      get "/api/v1/forecast?location=denver,co"
 
       response_hash = JSON.parse(response.body, symbolize_names: true)
       data = response_hash[:data]
       forecast = data[:attributes]
+      
       expect(data.keys).to include(:id, :type, :attributes)
       expect(forecast.keys).to include(:current_weather, :daily_weather, :hourly_weather)
       expect(forecast.keys).to_not include(:minutely, :alerts)
       expect(forecast[:current_weather].keys.length).to eq(10)
       expect(forecast[:current_weather].keys).to include(:datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon)
-      expect(forecast[:daily_weather].keys.length).to eq(7)
-      expect(forecast[:daily_weather].keys).to include(:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon)
-      expect(forecast[:hourly_weather].keys.length).to eq(4)
-      expect(forecast[:hourly_weather].keys).to include(:time, :temperature, :conditions, :icon)
+      expect(forecast[:daily_weather].length).to eq(5)
+      expect(forecast[:daily_weather][0].keys.length).to eq(7)
+      expect(forecast[:daily_weather][0].keys).to include(:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon)
+      expect(forecast[:hourly_weather].length).to eq(8)
+      expect(forecast[:hourly_weather][0].keys.length).to eq(4)
+      expect(forecast[:hourly_weather][0].keys).to include(:time, :temperature, :conditions, :icon)
     end
   end
 end
