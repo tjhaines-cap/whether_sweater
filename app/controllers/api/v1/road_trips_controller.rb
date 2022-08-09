c2ddad864e90d6a85eee9e7511a167c6ec0419e5
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-09 10:06:10 -0600

    Feat: use user serializer to render user info

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -2,12 +2,6 @@ class Api::V1::RoadTripsController < ApplicationController
 
   def create
     if valid_api_key?(params[:api_key]) || User.find_by(api_key: params[:api_key])
-      # travel = MapFacade.get_travel_time(params[:origin], params[:destination])
-      # hours = travel.split[0].to_i
-      # lat_lng = MapFacade.get_latitude_longitude(params[:destination])
-      # weather_after_travel = WeatherFacade.get_weather_summary_at_eta(lat_lng[:latitude], lat_lng[:longitude], hours)
-      # road_trip = RoadTrip.new(params[:origin], params[:destination], travel, weather_after_travel)
-      # render json: {data: {id: nil, type: "roadtrip", attributes: {start_city: params[:origin], end_city: params[:destination], travel_time: travel, weather_at_eta: weather_after_travel}}}
       road_trip = RoadTripFacade.get_roadtrip_info(params[:origin], params[:destination])
       render json: {data: road_trip}
     else
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params)
     # binding.pry
     if user.save      
-      render status: 201, json: {data: {type: "users", id: user.id, attributes: {email: user.email, api_key: user.api_key}}}
+      # render status: 201, json: {data: {type: "users", id: user.id, attributes: {email: user.email, api_key: user.api_key}}}
+      render status: 201, json: UserSerializer.format_user(user)
     elsif user.errors.full_messages == ['Email has already been taken']
       render json: { error: 'That email is already in use! Please try again with a unique email.'}, status: 400
     elsif user.errors.full_messages == ["Email can't be blank"]
