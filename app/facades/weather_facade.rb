2dcbf737f73f373eeab99d401b3f50c84a2dc1f3
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-08 20:24:36 -0600

    Feat: weather facade method to get weather a certain number of hours from now

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -4,4 +4,22 @@ class WeatherFacade
     weather_json = WeatherService.get_weather(latitude, longitude)
     Weather.new(weather_json)
   end 
+
+  def self.get_weather_summary_at_eta(latitude, longitude, hours)
+    weather_json = WeatherService.get_weather(latitude, longitude)
+    if hours <= 48
+      summary = {
+        temperature: weather_json[:hourly][hours][:temp],
+        conditions: weather_json[:hourly][hours][:weather][0][:description]
+      }
+      return summary
+    else
+      days = hours / 24
+      summary = {
+        temperature: weather_json[:daily][days][:temp][:day],
+        conditions: weather_json[:daily][days][:weather][0][:description]
+      }
+      return summary
+    end
+  end
 end
--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -14,7 +14,13 @@ RSpec.describe WeatherFacade do
     weather_at_eta = WeatherFacade.get_weather_summary_at_eta(38.8576, 104.9128, 30)
 
     expect(weather_at_eta.keys.length).to eq(2)
-    expect(weather_at_eta[:temperature]).to be_a(float)
-    expect(weather_at_eta[:description]).to be_a(String)
+    expect(weather_at_eta[:temperature]).to be_a(Float)
+    expect(weather_at_eta[:conditions]).to be_a(String)
+
+    weather_at_eta = WeatherFacade.get_weather_summary_at_eta(38.8576, 104.9128, 80)
+
+    expect(weather_at_eta.keys.length).to eq(2)
+    expect(weather_at_eta[:temperature]).to be_a(Float)
+    expect(weather_at_eta[:conditions]).to be_a(String)
   end
 end
